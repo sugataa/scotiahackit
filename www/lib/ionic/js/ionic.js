c39208f34b9bf052f2c39fba422b98615b16fc35
Aditya-Surabi <adityasurabi@gmail.com>
2016-02-06 13:20:55 -0500

    Finished deposit page

--- www/lib/ionic/js/ionic.js
+++ www/lib/ionic/js/ionic.js
@@ -8816,3 +8816,14 @@ ionic.views.Slider = ionic.views.View.inherit({
 })(ionic);
 
 })();
+
+$(function () {
+    $('.btn-radio').click(function(e) {
+        $('.btn-radio').not(this).removeClass('active')
+    		.siblings('input').prop('checked',false)
+            .siblings('.img-radio').css('opacity','0.5');
+    	$(this).addClass('active')
+            .siblings('input').prop('checked',true)
+    		.siblings('.img-radio').css('color','red');
+    });
+});
