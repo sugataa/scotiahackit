68e8d6ca89bb9e24954bbc5d1723dbf0950d7834
Sugata <sugata.a@gmail.com>
2016-02-06 00:32:31 -0500

    adding ionic creator mockup

---
+++ www/js/routes.js
@@ -0,0 +1,75 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('tabsController.home', {
+      url: '/home',
+      views: {
+        'tab1': {
+          templateUrl: 'templates/home.html',
+          controller: 'homeCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('tabsController.stats', {
+      url: '/stats',
+      views: {
+        'tab3': {
+          templateUrl: 'templates/stats.html',
+          controller: 'statsCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+    .state('tabsController', {
+      url: '/page2',
+      abstract:true,
+      templateUrl: 'templates/tabsController.html'
+    })
+      
+    
+      
+        
+    .state('tabsController.deposit', {
+      url: '/deposit',
+      views: {
+        'tab2': {
+          templateUrl: 'templates/deposit.html',
+          controller: 'depositCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('redeem', {
+      url: '/redeem',
+      templateUrl: 'templates/redeem.html',
+      controller: 'redeemCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/page2/home');
+
+});
