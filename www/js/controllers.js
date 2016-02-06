96266640e93658873ee7baf398075e4fbf371c3d
Sugata <sugata.a@gmail.com>
2016-02-06 09:38:01 -0500

    adding login screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,4 +15,19 @@ angular.module('app.controllers', [])
 .controller('redeemCtrl', function($scope) {
 
 })
+
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+    $scope.data = {};
+ 
+    $scope.login = function() {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            $state.go('tab.dash');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+    }
+})
  
